964da15b2d75d7c494c3ae6a8412a2204abfd801
lyooyl <69505403+lyooyl@users.noreply.github.com>
2023-07-13 17:25:34 +0800

    Update 04_yolo_det.sh
    指定 --classes 0，仅检测person类

--- process_src/04_yolo_det.sh
+++ process_src/04_yolo_det.sh
@@ -2,6 +2,7 @@
 /home/lyo/miniconda3/envs/py38/bin/python ./process_src/yolovDeepsort/yolov5/detect.py \
 --weights ./process_src/yolovDeepsort/yolov5/yolov5s.pt \
 --source ./process_file/choose_frames_all/ \
+--classes 0 \
 --save-txt \
 --save-conf \
 --project ./process_file \
