066ab66e6e82db4487587a49aa62d6104f8ed383
lyooyl <69505403+lyooyl@users.noreply.github.com>
2023-07-13 22:58:40 +0800

    Update train.py
    在修复CSV中未识别的“-1”时，解决最后一条的索引超限问题

--- process_src/train.py
+++ process_src/train.py
@@ -25,13 +25,15 @@ def update_train_temp(videoName, index, maxId):
                 x2 = float(train_temp[index][4])
                 y2 = float(train_temp[index][5])
                 for index3 in range(10):
+                    if index + index3 + 1 >= len(train_temp):
+                        continue
                     if train_temp[index + index3 + 1][-1] == '-1':
                         xT1 = float(train_temp[index + index3 + 1][2])
                         yT1 = float(train_temp[index + index3 + 1][3])
                         xT2 = float(train_temp[index + index3 + 1][4])
                         yT2 = float(train_temp[index + index3 + 1][5])
-                        if abs(x1 - xT1) < 0.005 and abs(y1 - yT1) < 0.005 and abs(x2 - xT2) < 0.005 and abs(
-                                y2 - yT2) < 0.005:
+                        if abs(x1 - xT1) < 0.01 and abs(y1 - yT1) < 0.01 and abs(x2 - xT2) < 0.01 and abs(
+                                y2 - yT2) < 0.01:
                             train_temp[index + index3 + 1][-1] = maxId + 1
                         else:
                             break
