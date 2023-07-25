ce3028b13c90a332b15719f67320502e6717fdbd
lyooyl <69505403+lyooyl@users.noreply.github.com>
2023-07-25 09:52:35 +0800

    Update 01_cut_video.sh

--- process_src/01_cut_video.sh
+++ process_src/01_cut_video.sh
@@ -6,26 +6,26 @@ if [ ! -d "${OUT_DATA_DIR}" ]; then
   # 创建路径
   mkdir -p "${OUT_DATA_DIR}"
 fi
-
-#ffmpeg -i ${IN_DATA_DIR}/drink_22.mp4 -r 30 -ss 0 -t 11 ${OUT_DATA_DIR}/22.mp4 -y
-#ffmpeg -i ${IN_DATA_DIR}/drink_49.mp4 -r 30 -vf "setpts=(PTS-STARTPTS)*(11/10)" -c:a copy ${OUT_DATA_DIR}/49.mp4
-
 # 设置要获取文件名的目录路径
 files=$(ls ${IN_DATA_DIR})
 # 循环遍历
+idx=0
 for file in $files
 do
-    NEW_NAME=$(cut -d'_' -f2 <<< ${file})
+    echo "正在处理${file}"
+    # NEW_NAME=$(cut -d'_' -f2 <<< ${file})
+    NEW_NAME=$(printf "%02d" $idx).mp4
     LEN_TIME=$(ffprobe -i ${IN_DATA_DIR}/${file} -show_entries format=duration -v quiet -of csv="p=0")
-    comparison=$(echo "$LEN_TIME < 11.0" | bc)
-    if [ $comparison -eq 1 ]; then
-      echo "时长小于11秒"
-      ffmpeg -i ${IN_DATA_DIR}/${file} -vf "setpts=PTS*2" -r 30 -ss 0 -t 11 ${OUT_DATA_DIR}/${NEW_NAME} -y
-    else
-      ffmpeg -i ${IN_DATA_DIR}/${file} -r 30 -ss 0 -t 11 ${OUT_DATA_DIR}/${NEW_NAME} -y
-    fi
+    k=11.0/$LEN_TIME
+    ffmpeg -i ${IN_DATA_DIR}/${file} -vf "setpts=PTS*$k" -r 30 -ss 0 -t 11 ${OUT_DATA_DIR}/${NEW_NAME} -y
+    # if [ $(echo "$LEN_TIME < 11.0" | bc) -eq 1 ]; then
+    #   echo "时长小于11秒"
+    #   ffmpeg -i ${IN_DATA_DIR}/${file} -vf "setpts=PTS*$k" -r 30 -ss 0 -t 11 ${OUT_DATA_DIR}/${NEW_NAME} -y
+    # else
+    #   ffmpeg -i ${IN_DATA_DIR}/${file} -r 30 -ss 0 -t 11 ${OUT_DATA_DIR}/${NEW_NAME} -y
+    # fi
+    idx=$((idx + 1))
 done
-
 
 
 
